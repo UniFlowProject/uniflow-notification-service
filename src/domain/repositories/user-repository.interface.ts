db80523a6f9301b8cf9d0ea3a08cf72c619a90f0
FabiSax12 <fabidev18@gmail.com>
2026-07-15 23:25:52 -0600

    feat: remove IUserRepository interface as part of refactoring

--- src/domain/repositories/user-repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../entities/user';
-import { UserId } from '../value-objects/user-id';
-import { Email } from '../value-objects/email';
-
-export interface IUserRepository {
-  findById(id: UserId): Promise<User | null>;
-  findByEmail(email: Email): Promise<User | null>;
-  save(user: User): Promise<User>;
-}
